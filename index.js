b958b9cb1f20d874cc988dc880ca41e77c9973f6
Waqas Ehmed <waqasehmed1@gmail.com>
2021-03-24 06:01:46 +0500

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 8092;
+const app = express();
+
+
+app.get('/', ({ req, res }) => {
+    res.send(`app is runing at ${port}`);
+})
+
 app.listen(port, () => { console.log(`App is up and running @ ${port}`) })
